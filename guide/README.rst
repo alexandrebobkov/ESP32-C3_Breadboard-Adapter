68437c0366442d1527b96cfcc39e88a8ddb1e72a
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-10-05 05:41:40 -0400

    guide

---
+++ guide/README.rst
@@ -0,0 +1,7 @@
+About
+======
+
+**ESP32-C3 Breadboard Power Adapter** is a combo PCB that comes with a power supply and on-board ESP32-C3 microcontroller.
+
+.. note::
+    This project is under active development.
--- guide/conf.py
+++ guide/conf.py
@@ -6,16 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ESP32-C3-BreadBoard-Adapter'
+project = 'ESP32-C3 BreadBoard Adapter'
 copyright = '2024, Alexander Bobkov'
 author = 'Alexander Bobkov'
+version = 'v1.0'
 release = '2.0'
+today = '%d %b, %Y'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.viewcode']
 
+source_suffix = '.rst'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -26,3 +30,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    footer = 'Alexander Bobkov',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+latex_show_urls = 'footnote'
