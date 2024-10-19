a70af19ea51d46b921d4b27c6770f30342e9364a
Alexandre Bobkov <alexandre.bobkov@gmail.com>
2024-10-19 02:47:54 -0400

    manual

--- guide/source/conf.py
+++ guide/source/conf.py
@@ -19,6 +19,29 @@ extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode','sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Options for PDF output 
+latex_engine = 'xelatex'
+latex_elements = {
+    'passoptionstopackages': r'''
+\PassOptionsToPackage{svgnames}{xcolor}
+''',
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'sphinxsetup': 'TitleColor=DarkGoldenrod',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+
 
 
 # -- Options for HTML output -------------------------------------------------
